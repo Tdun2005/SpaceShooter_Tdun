53f89cdfa8c69e4c333494eb23f40bb08f0bc11b
Tdun2005 <lqd9a1tungduongphamnguyen@gmail.com>
2026-03-22 13:44:44 +0700

    Update part 8

---
+++ Assets/Scripts/FlyPath.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class FlyPath : MonoBehaviour
+{
+    public Transform[] waypoints;
+
+    // cho phép truy cập kiểu array
+    public Vector3 this[int index] => waypoints[index].position;
+
+    private void OnDrawGizmos()
+    {
+        if (waypoints == null || waypoints.Length < 2) return;
+
+        Gizmos.color = Color.green;
+
+        for (int i = 0; i < waypoints.Length - 1; i++)
+        {
+            Gizmos.DrawLine(
+                waypoints[i].position,
+                waypoints[i + 1].position
+            );
+        }
+    }
+}
---
+++ Assets/Scripts/FlyPathAgent.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+public class FlyPathAgent : MonoBehaviour
+{
+    public FlyPath flyPath;
+    public float flySpeed = 3f;
+
+    private int nextIndex = 1;
+
+    void Start()
+    {
+        if (flyPath != null)
+        {
+            transform.position = flyPath[0];
+        }
+    }
+
+    void Update()
+    {
+        if (flyPath == null) return;
+
+        if (nextIndex >= flyPath.waypoints.Length)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        if (Vector3.Distance(transform.position, flyPath[nextIndex]) > 0.05f)
+        {
+            FlyToNextWaypoint();
+            LookAt(flyPath[nextIndex]);
+        }
+        else
+        {
+            nextIndex++;
+        }
+    }
+
+    void FlyToNextWaypoint()
+    {
+        transform.position = Vector3.MoveTowards(
+            transform.position,
+            flyPath[nextIndex],
+            flySpeed * Time.deltaTime
+        );
+    }
+
+    void LookAt(Vector2 destination)
+    {
+        Vector2 position = transform.position;
+        Vector2 dir = destination - position;
+
+        if (dir.magnitude < 0.01f) return;
+
+        float angle = Vector2.SignedAngle(Vector2.down, dir);
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
+}
